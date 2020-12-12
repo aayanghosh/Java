47e4a8e167b3f0bf2cfc905501e714b29dc8538d
aayanghosh <aayanghosh1122@gmail.com>
2020-12-12 10:16:18 -0500

    first program

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.aayan.example;
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; 
+
+    System.out.println(myDouble);   
+    System.out.println(myInt);      
+    
+    int w = 22;
+    char e = (char) w;
+    System.out.println(e);
+  }
+}
